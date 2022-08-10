5a3b38ecaddc772966ff9b8873ba4d6f499cb9ea
My Name <shibli.karatbars@gmail.com>
2022-08-10 15:41:25 +0600

    Posgresql add

--- project/settings.py
+++ project/settings.py
@@ -101,8 +101,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kbproject',
+        'USER': 'postgres',
+        'PASSWORD': 'KB20ks21SL8eaf56',
+        'HOST': 'localhost',
     }
 }
 
